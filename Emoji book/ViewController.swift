ae9e36deb1fe98f11287ee515807686c9191cb75
Timothy Sparrow <timbospaz@gmail.com>
2017-01-24 17:33:45 +0100

    emoji now seeable

--- Emoji book/ViewController.swift
+++ Emoji book/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,  UITableViewDataSource,
+    UITableViewDelegate {
 
+    @IBOutlet weak var cooltableview: UITableView!
+    
+    var emojis = ["😊","💩","😤","👮","🐰","🐺"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        cooltableview.dataSource = self
+        cooltableview.delegate  = self
+        
     }
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+               let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
