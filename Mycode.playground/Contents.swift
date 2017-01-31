f3a95ee7ca47ea312f013497bf5204a8cc5a559e
Timothy Sparrow <timbospaz@gmail.com>
2017-01-31 21:46:30 +0100

    plus some other stuff too

---
+++ Mycode.playground/Contents.swift
@@ -0,0 +1,17 @@
+
+
+
+func personInfo (name:String,age:Int,weight:Double) ->
+
+    
+    String {
+    return "My name is \(name),  i am \(age) years of age and weigh \(weight)lbs  "
+}
+
+print (personInfo(name: "Tim", age:49, weight: 45.5))
+
+print (personInfo(name: "Heidi", age:47, weight: 44.5))
+
+
+
+
